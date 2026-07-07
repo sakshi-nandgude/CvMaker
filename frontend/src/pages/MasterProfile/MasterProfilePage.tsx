51af96ebdfcb6fa70a1fb8edc314a0fe6aaaa5c2
sakshinandgude6-web <sakshinandgude6@gmail.com>
2026-07-07 16:39:45 +0100

    modified: MasterProfilePage.tsx

--- frontend/src/pages/MasterProfile/MasterProfilePage.tsx
+++ frontend/src/pages/MasterProfile/MasterProfilePage.tsx
@@ -1,22 +1,16 @@
-import MainLayout from "../../components/layout/MainLayout";
 import PageHeader from "../../components/common/PageHeader";
-
 import PersonalProfileForm from "../../features/profile/components/PersonalProfileForm";
-import ExperienceSection from "../../features/experience/components/ExperienceSection";
 
 function MasterProfilePage() {
   return (
-    <MainLayout>
+    <>
       <PageHeader
         title="Master Profile"
         subtitle="Manage your professional information."
       />
 
       <PersonalProfileForm />
-      <div className="mt-8">
-    <ExperienceSection />
-</div>
-    </MainLayout>
+    </>
   );
 }
 
